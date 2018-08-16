911a66ae5be1cfdf15e9ad66ca10b76433e377c9
Cleverson <cleverson_reisferreira@hotmail.com>
2018-08-16 18:17:31 -0300

    first commi

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,15 @@ use Illuminate\Http\Request;
 //login
 Route::post('login', 'UserController@apiToken');
 
-//all boards
-Route::get('boards', function (){
-    $boards = \App\Board::all();
-    return \App\Http\Resources\BoardResource::collection($boards);
-});
+Route::group(['middleware' => 'auth:api'], function (){
+    //all boards
+    Route::get('boards', function (){
+        $boards = \App\Board::all();
+        return \App\Http\Resources\BoardResource::collection($boards);
+    });
 
-//board
-Route::get('board/{board}', function (\App\Board $board){
-    return new \App\Http\Resources\BoardResource($board);
+    //board
+    Route::get('board/{board}', function (\App\Board $board){
+        return new \App\Http\Resources\BoardResource($board);
+    });
 });
